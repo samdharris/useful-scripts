954a878c56f7f5b68ec80d31504d35e31e1a3cd6
Sam D Harris <samdharris@outlook.com>
2020-03-16 17:48:02 +0000

    Add repo branch cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+git remote -v 2>/dev/null
+resp=$?
+if [[ $resp -ne 0 ]]
+then
+	echo "$(pwd) is not a git repository! exiting..."
+	exit 1
+fi
+
+branches="$(git branch --merged)"
+for b in $branches
+do
+    if [$b = 'master']
+    then
+        continue
+    fi
+
+    if [$b = 'dev']
+    then
+        continue
+    fi
+
+    echo "deleting branch $b"
+    git branch -d -r $b
+    git fetch --prune
+done
